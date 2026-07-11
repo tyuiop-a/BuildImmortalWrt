074b5b292eed1e34f47a26fc2fb0722905434697
tyuiop-a <1130198145@qq.com>
2026-07-11 17:22:21 +0800

    Update build25.sh

--- mediatek-filogic/build25.sh
+++ mediatek-filogic/build25.sh
@@ -44,8 +44,8 @@ echo "$(date '+%Y-%m-%d %H:%M:%S') - Starting build process..."
 
 # 定义所需安装的包列表 下列插件你都可以自行删减
 PACKAGES=""
-#PACKAGES="$PACKAGES curl luci luci-i18n-base-zh-cn"
-#PACKAGES="$PACKAGES luci-i18n-firewall-zh-cn"
+PACKAGES="$PACKAGES curl luci luci-i18n-base-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-firewall-zh-cn"
 PACKAGES="$PACKAGES luci-theme-argon"
 PACKAGES="$PACKAGES luci-app-argon-config"
 PACKAGES="$PACKAGES luci-i18n-argon-config-zh-cn"
